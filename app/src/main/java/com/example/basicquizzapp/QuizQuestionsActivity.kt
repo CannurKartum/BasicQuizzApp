3b39e16bb84ceea490e9c03977edec880322a545
Cannur Kartum <48458722+CannurKartum@users.noreply.github.com>
2024-06-22 19:55:43 +0200

    adding questions and ui

--- app/src/main/java/com/example/basicquizzapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/basicquizzapp/QuizQuestionsActivity.kt
@@ -1,6 +1,10 @@
 package com.example.basicquizzapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +12,18 @@ import androidx.core.view.WindowInsetsCompat
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+         var progressBar : ProgressBar? = null
+         var tvProgress: TextView? = null
+         var tvQuestion: TextView? = null
+         var tvImage: ImageView? = null
+         var optionOne: TextView? = null
+        var optionTwo: TextView? = null
+        var optionThree: TextView? = null
+        var optionFour: TextView? = null
+
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_quiz_questions)
@@ -16,8 +32,28 @@ class QuizQuestionsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        progressBar =  findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tvProgress)
+        tvQuestion = findViewById(R.id.tvQuestion)
+        tvImage = findViewById(R.id.tvImage)
+        optionOne = findViewById(R.id.optionOne)
+        optionTwo = findViewById(R.id.optionTwo)
+        optionThree = findViewById(R.id.optionThree)
+        optionFour = findViewById(R.id.optionFour)
 
+        val  questionsList = Constants.getQuestions()
+        Log.i("QuestionsList", "${questionsList.size}")
 
+        var currentPosition = 1
+        val question : Question = questionsList[currentPosition - 1]
+        tvImage?.setImageResource(question.image)
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        tvQuestion?.text = question.question
+        optionOne?.text = question.optionOne
+        optionTwo?.text = question.optionTwo
+        optionThree?.text = question.optionThree
+        optionFour?.text = question.optionFour
 
 
     }
